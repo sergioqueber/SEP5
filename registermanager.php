b169327e6674848e2b95a2d78b20e2a6d7aee4b3
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-06 14:59:03 +0100

    Forms

---
+++ registermanager.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Register</h1>
+
+<form action="includes/registermanagerhandler.inc.php" method="post">
+    <input type="text" name="username" placeholder="Username"><br><br>
+    <input type="password" name="password" placeholder="Password"><br><br>
+    <input type="text" name="email" placeholder="Email"><br><br>
+    <input type="text" name="city" placeholder="City"><br><br>
+    <input type="text" name="street" placeholder="Street"><br><br>
+    <input type="text" name="houseNo" placeholder="House Number"><br><br>
+    <input type="text" name="flatNo" placeholder="Flat Number"><br><br>
+    <input type="text" name="fName" placeholder="First name"><br><br>
+    <input type="text" name="lName" placeholder="Last name"><br><br>
+    <input type="text" name="cpr" placeholder="CPR"><br><br>
+    <input type="text" name="phoneNo" placeholder="Phone No"><br><br>
+    <input type="text" name="postcode" placeholder="Post No"><br><br>
+
+
+    <button>Log in</button>
+</form>
+</body>
+</html>
