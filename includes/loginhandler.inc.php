8458ed1e57e67ef50ac732a844f542ca5c7961a9
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-06 10:11:06 +0100

    Log in

---
+++ includes/loginhandler.inc.php
@@ -0,0 +1,34 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $username = $_POST["username"];
+    $psw = $_POST["password"];
+   
+
+    try {
+        require_once "dbh.inc.php";
+
+        $query = "SELECT password FROM customer WHERE username = :username;";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':username', $username);
+        $stmt->execute();
+
+        $results = $stmt->fetchColumn();
+        echo $results;
+        echo $psw;
+        if($results == $psw){
+            echo 'Log in successful';
+            header("Location: ../index.php");
+        }
+        else{
+            echo 'log in failed';
+        }
+
+        die();
+
+    } catch (PDOException $e) {
+        die("Query failed" . $e->getMessage());
+    }
+}else{
+    header("Location: ../index.php");
+};
