c8a14a28abd19e0a042dea0d72c099de561a9595
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-07 12:18:43 +0100

    Nav bar

--- product.php
+++ product.php
@@ -9,6 +9,23 @@
 
 <body>
 
+    <nav class="navbar navbar-custom navbar-expand-sm navbar-light fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">Sergio Berdonce</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar"> <span class="navbar-toggler-icon"> </span></button>
+            </div>
+            <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li><a class="nav-link active" href="">Home </a></li>
+                    <li><a class="nav-link" href="My_town.html">Products </a></li>
+                    <li><a class="nav-link" href="Education.html">About us</a></li>
+                    <a href="login.php"><img src="Images/profileorange 1.png" alt="Profile pic"></a>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
     <?php
     $productId = isset($_GET['id']) ? $_GET['id'] : null;
     session_start();
