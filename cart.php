7edd765d8bee7beed2affd586eef9220630d3b26
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-08 13:05:30 +0100

    Cart to order wip

--- cart.php
+++ cart.php
@@ -98,6 +98,26 @@
             echo "</div>";
         }
     }
+
+    $query = "INSERT INTO order(username) VALUES (?) RETURNING order_id;";
+    $stmt = $pdo->prepare($query);
+    $stmt->execute([$username]);
+    
+    $query = "SELECT product_id, quantity
+    FROM cart_item
+             JOIN blocal.cart c ON cart_item.cart_id = c.cart_id
+    WHERE username = ?;";
+    $stmt = $pdo->prepare($query);
+    $stmt->execute([$username]);
+    $cartItems = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $query = "INSERT INTO order_item(product_id, order_id, quantity) VALUES (?,?,?);";
+    $stmt = $pdo->prepare($query);
+    
+    foreach ($cartItems as $cartItem) {
+        $stmt->execute([$cartItem['product_id', ]]);
+    }
+   
     ?>
 
     <button>Place Order</button>
