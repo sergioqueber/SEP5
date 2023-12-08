49cb94eb18e7507d00fb79287d026890727a5617
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-08 12:40:00 +0100

    a

--- includes/sendmessage.inc.php
+++ includes/sendmessage.inc.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+/*session_start();
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $username = $_SESSION['username'];
     $message = $_POST["message"];
@@ -18,6 +18,30 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         
        
 
+
+    } catch (PDOException $e) {
+        die("Query failed" . $e->getMessage());
+    }
+}else{
+    header("Location: ../index.php");
+};*/
+
+session_start();
+if(isset($_POST['message'])){
+    $username = $_SESSION['username'];
+    $message = $_POST['message'];
+    $direction = TRUE;
+   
+
+    try {
+        require_once "dbh.inc.php";
+
+        $query = "INSERT INTO message (message, direction, username, store_id) VALUES (?, ?, ?, ?);";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([$message, $direction, $username, 1]);
+        
+
+        die();
 
     } catch (PDOException $e) {
         die("Query failed" . $e->getMessage());
