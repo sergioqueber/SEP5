69d21b5f78ca201c5d4a887301881ca692f2c605
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-08 07:49:20 +0100

    Nav bar

--- index.php
+++ index.php
@@ -47,7 +47,40 @@
                 </ul>
             </div>
         </div>
-    </nav>
+    </nav><nav class="navbar navbar-custom navbar-expand-sm navbar-light fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">
+                <img src="Images/Blocal logo.png" width="30" height="30" alt="logo" class="img-fluid">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+
+        <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <li><a class="nav-link active" href="">Home</a></li>
+                <li><a class="nav-link" href="My_town.html">Products</a></li>
+                <li><a class="nav-link" href="Education.html">About us</a></li>
+
+                <!-- Dropdown Button and Menu -->
+                <li class="nav-item dropdown">
+                    <button type="button" class="btn" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img src="Images/profileorange 1.png" alt="Profile pic">
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-right">
+                        <a class="dropdown-item" href="#">Profile</a>
+                        <a class="dropdown-item" href="#">Settings</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Logout</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
 
     <form action="includes/formhandler.inc.php" method="post">
         <input type="text" name="productName" placeholder="Product Name"><br>
