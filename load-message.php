675e4228ffcb0637f46209f714d2895427a127c6
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-08 12:39:11 +0100

    a

--- load-message.php
+++ load-message.php
@@ -1,11 +1,39 @@
 <?php
-session_start();
+/*session_start();
 $username = $_SESSION['username'];
 try {
             require_once "includes/dbh.inc.php";
             $messagesNewCount = $_POST['messagesNewCount'];
 
             $query = "SELECT * FROM message WHERE username = ? ORDER BY message_id DESC LIMIT $messagesNewCount;";
+
+            $stmt = $pdo->prepare($query);
+            $stmt->execute([$username]);
+    
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            if(empty($results)){
+                echo "<br>";
+                echo "No messages yet :(";
+            }
+            else{
+                foreach($results as $row){
+                    echo "<p>";
+                    echo $username;
+                    echo "<br>";
+                    echo $row['message'];
+                    echo "<p>";
+                }
+            }
+        } catch (PDOException $e) {
+            die("Query failed: " . $e->getMessage());
+        }*/
+session_start();
+$username = $_SESSION['username'];
+try {
+            require_once "includes/dbh.inc.php";
+            $messagesNewCount = $_POST['messagesNewCount'];
+
+            $query = "SELECT * FROM message WHERE username = ? LIMIT $messagesNewCount;";
 
             $stmt = $pdo->prepare($query);
             $stmt->execute([$username]);
