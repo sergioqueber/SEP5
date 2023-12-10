857dc4886ee530e12999d8193053f985abcd1ed3
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-11 00:31:32 +0100

    q

--- includes/additemhandler.inc.php
+++ includes/additemhandler.inc.php
@@ -1,7 +1,8 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     session_start();
-    $productID = $_POST['productId'];
+    $productID = $_POST["productId"];
+    echo $productID;
     try {
         require_once "dbh.inc.php";
         $query = "INSERT INTO cart_item(cart_id, product_id, quantity) VALUES (?,?,?);";
@@ -14,6 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $stmt = null;
     } catch (\Throwable $th) {
         echo $productID;
+
         $query = "UPDATE cart_item SET quantity = quantity+1 WHERE product_id = ? AND cart_id = ?;";
         $stmt = $pdo->prepare($query);
         $stmt->execute([$productID, $_SESSION['cart']]);
