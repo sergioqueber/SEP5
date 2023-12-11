aa6ff4e2a77611676b3cb2837fd70c744b60602a
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-11 11:53:24 +0100

    carousel

--- index.php
+++ index.php
@@ -180,12 +180,12 @@
     </div>    
     <div class="carousel-inner">
         <!-- Slide 1 -->
-        <div class="carousel-item active">
+        <div class="carousel-item active d-flex align-items-center justify-content-center">
             <img src="Images/rubiostus logo.png" class="d-block w-50 img-fluid" alt="Image 1">
         </div>
 
         <!-- Slide 2 -->
-        <div class="carousel-item">
+        <div class="carousel-item d-flex align-items-center justify-content-center">
             <img src="Images/Logo.png" class="d-block w-50 img-fluid" alt="Image 2">
         </div>
     </div>
