3047c76430ea4e444f7aab0c0162f8bc01b4fd41
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-11 12:06:01 +0100

    Carousel

--- index.php
+++ index.php
@@ -182,17 +182,17 @@
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="Images/rubiostus logo.png" class="d-block w-100" alt="Image 1">
+      <img src="Images/rubiostus logo.png" class="d-block w-50 img-fluid" alt="Image 1">
     </div>
     <div class="carousel-item">
-      <img src="Images/Logo.png" class="d-block w-100" alt="Image 2">
+      <img src="Images/Logo.png" class="d-block w-50 img-fluid" alt="Image 2">
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
