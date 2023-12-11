cecdafdcd0470ddd04ba8f6b15e789090689f346
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-11 17:58:41 +0100

    Total

--- includes/placeorderhandler.inc.php
+++ includes/placeorderhandler.inc.php
@@ -9,20 +9,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $stmt->execute([$username]);
         $orderId = $stmt->fetchColumn();
         
-        $query = "SELECT product_id, quantity
-            FROM cart_item
-                    JOIN blocal.cart c ON cart_item.cart_id = c.cart_id
-            WHERE username = ?;";
+        $query = "SELECT cart_item.product_id, quantity, price
+        FROM cart_item
+                 JOIN blocal.cart c ON cart_item.cart_id = c.cart_id
+                 JOIN blocal.product p ON cart_item.product_id = p.product_id
+        WHERE username = ?;";
         $stmt = $pdo->prepare($query);
         $stmt->execute([$username]);
         $cartItems = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         $query = "INSERT INTO order_item(product_id, order_id, quantity) VALUES (?,?,?);";
         $stmt = $pdo->prepare($query);
-        
+        $totalprice = 0;
         foreach ($cartItems as $cartItem) {
             $stmt->execute([$cartItem['product_id'], $orderId,$cartItem['quantity']]);
+            $totalprice = $totalprice + ($cartItem['price']*$cartItem['quantity']);
         };
+
+        $query = 'UPDATE "order" SET total_price = ? WHERE order_id = ?;';
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([$totalprice,$orderId ]);
 
         $query = "DELETE FROM cart_item WHERE cart_id = ? ";
         $stmt = $pdo->prepare($query);
