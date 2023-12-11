aaf3fbbdc765023e448122a7c89f3d8eb52202a0
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-11 16:25:22 +0100

    Now

--- includes/formhandler.inc.php
+++ includes/formhandler.inc.php
@@ -18,7 +18,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
         $pdo = null;
         $stmt = null;
 
-        header("Location: ../README.php");
+        header("Location: ../mainpagemanager.php");
 
         die();
     } catch (PDOException $e) {
