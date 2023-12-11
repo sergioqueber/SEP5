ba165111fb7bce25e19b245fa85baeab5fd32173
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-11 11:46:37 +0100

    Carousel

--- index.php
+++ index.php
@@ -174,6 +174,10 @@
 </div>
 
 <div id="imageCarousel" class="carousel carousel-dark slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#imageCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#imageCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    </div>    
     <div class="carousel-inner">
         <!-- Slide 1 -->
         <div class="carousel-item active">
