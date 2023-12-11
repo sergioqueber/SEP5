44300bb959c0ed89ef373d23f2f0775cc5d3827e
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-11 02:10:05 +0100

    log in

---
+++ includes/loginemployee.inc.php
@@ -0,0 +1,40 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    session_start();
+    
+    $username = $_POST["username"];
+    $psw = $_POST["password"];
+    $_SESSION['username'] = $username;
+
+    try {
+        require_once "dbh.inc.php";
+
+        $query = "SELECT password FROM employee WHERE username = :username;";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':username', $username);
+        $stmt->execute();
+
+        $results = $stmt->fetchColumn();
+
+        $query = "SELECT store_id FROM employee WHERE username = ?;";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([$username]);
+        $_SESSION['store_id'] =  $stmt->fetchColumn();
+
+        echo $results;
+        echo $psw;
+        if ($results == $psw) {
+            echo 'Log in successful';
+            header("Location: ../index.php");
+        } else {
+            echo 'log in failed';
+        }
+
+        die();
+    } catch (PDOException $e) {
+        die("Query failed" . $e->getMessage());
+    }
+} else {
+    header("Location: ../index.php");
+};
