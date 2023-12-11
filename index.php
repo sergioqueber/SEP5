9c2005a4932b3817b7de83437687cefa001f88a5
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-11 12:17:55 +0100

    a

--- index.php
+++ index.php
@@ -165,7 +165,8 @@
     </div>
 </nav>
 <div class = 'container'>
-    <div class = 'row mt-5 margin'>
+    <div class = 'row margin'></div>
+    <div class = 'row mt-5'>
         <div class = 'column-md text-center'>
             <h1 id = 'slogan'>Small shops, big dreams</h1>
             <h1 id = 'slogan2'>Transforming local economies together</h1>
