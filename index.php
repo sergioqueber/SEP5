2ec9257bf8b113187d80b1412e25cb0ae18e6568
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-11 11:28:54 +0100

    main page

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -13,13 +13,13 @@
 <body>
 <h1></h1>
     <?php
-    session_start();
+   /*  session_start();
     if (extension_loaded('pgsql')) {
         echo 'PostgreSQL extension is loaded.';
     } else {
         echo 'PostgreSQL extension is not loaded.';
     }
-
+ */
     ?>
 
 <nav class="navbar navbar-custom navbar-expand-sm navbar-light fixed-top">
@@ -46,13 +46,13 @@
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <img src="Images/profileorange 1.png" alt="Profile pic">
                         <?php
-                            
+                          /*   
                             if(session_status() == PHP_SESSION_ACTIVE && isset($_SESSION['username'])){
                                 
                                 echo 'Hi ' . $_SESSION['username'];
                             } else {
                                 echo 'Anonymus user';
-                            }
+                            } */
                         ?>
                     </a>
                     <div class="dropdown-menu dropdown-menu-right">
@@ -105,4 +105,73 @@
 
 </body>
 
+</html> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="CSS/styles.css" type="text/css">
+    <link href="CSS/boostrap/bootstrap.min.css" type="text/css" rel="stylesheet">
+    <script src="js/bootstrap.bundle.min.js"> </script>
+</head>
+<body>
+<nav class="navbar navbar-custom navbar-expand-sm navbar-light fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">
+                <img src="Images/Blocal logo.png" width="30" height="30" alt="logo" class="img-fluid">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+
+        <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <li><a class="nav-link active" href="">Home</a></li>
+                <li><a class="nav-link" href="">Products</a></li>
+                <li><a class="nav-link" href="">About us</a></li>
+                <li><a class="nav-link" href="wishlist.php">Wishlist</a></li>
+                <li><a class = "nav-link" href="cart.php">
+                    <img src="Images/cartbl 1.png" alt="Cart">
+                </a></li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img src="Images/profileorange 1.png" alt="Profile pic">
+                        <?php
+                            
+                            if(session_status() == PHP_SESSION_ACTIVE && isset($_SESSION['username'])){
+                                
+                                echo 'Hi ' . $_SESSION['username'];
+                            } else {
+                                echo 'Anonymus user';
+                            } 
+                        ?>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right">
+                        <a class="dropdown-item" href="#">Profile</a>
+                        <a class="dropdown-item" href="login.php">Log-in personal</a>
+                        <a class="dropdown-item" href="loginmanager.php">Log-in business manager</a>
+                        <a class="dropdown-item" href='loginemployee.php'>Log-in business employee</a>vscode-vfs://github%2B7b2276223a312c22726566223a7b2274797065223a342c226964223a224d617263696e227d7d/sergioqueber/SEP5/loginmanager.php vscode-vfs://github%2B7b2276223a312c22726566223a7b2274797065223a342c226964223a224d617263696e227d7d/sergioqueber/SEP5/loginemployee.php vscode-vfs://github%2B7b2276223a312c22726566223a7b2274797065223a342c226964223a224d617263696e227d7d/sergioqueber/SEP5/includes/loginmanagerhandler.inc.php vscode-vfs://github%2B7b2276223a312c22726566223a7b2274797065223a342c226964223a224d617263696e227d7d/sergioqueber/SEP5/includes/loginemployee.inc.php
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="includes/logout.inc.php">Log-out</a>
+                    </div>
+                </li>          
+                
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class = 'row mt-5'>
+    <div class = 'column-md text-center'>
+        <h1 id = 'slogan'>Small shops, bif dreams</h1>
+        <h1 id = 'slogan2'>Transforming local economies together</h1>
+        <a class="btn btn-primary" href="mainpagecustomer.php">Find your product</a>
+    </div>
+</div>
+</body>
 </html>
