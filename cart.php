edb856b06e0f3dcaa4355b15d630672b3452d812
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-11 01:26:53 +0100

    order

--- cart.php
+++ cart.php
@@ -100,45 +100,8 @@
 
    
     ?>
-    <script>
-        $(document).ready(function(){
-            $("button").click(function(){
-                $('#order').load();
-            })
-        })
-    </script>
-    <button>Place Order</button>
-    <div id = 'order'>
-        <?php
-            $query = 'INSERT INTO "order"(username) VALUES (?) RETURNING order_id;';
-            $stmt = $pdo->prepare($query);
-            $stmt->execute([$username]);
-            $orderId = $stmt->fetchColumn();
-            
-            $query = "SELECT product_id, quantity
-                FROM cart_item
-                        JOIN blocal.cart c ON cart_item.cart_id = c.cart_id
-                WHERE username = ?;";
-            $stmt = $pdo->prepare($query);
-            $stmt->execute([$username]);
-            $cartItems = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-            $query = "INSERT INTO order_item(product_id, order_id, quantity) VALUES (?,?,?);";
-            $stmt = $pdo->prepare($query);
-            
-            foreach ($cartItems as $cartItem) {
-                $stmt->execute([$cartItem['product_id'], $orderId,$cartItem['quantity']]);
-            };
-
-            $query = "DELETE FROM cart_item WHERE cart_id = ? ";
-            $stmt = $pdo->prepare($query);
-            $stmt->execute([$_SESSION['cart']]);
-
-            $pdo = null;
-            $stmt = null;
-            
-            echo 'Order placed'
-        ?>
-    </div>
+    <form action="includes/placeorderhandler.inc.php" method="post">
+        <button>Place Order</button>
+    </form>
 </body>
 </html>
---
+++ includes/placeorederhandler.inc.php
@@ -0,0 +1,44 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    session_start();
+    $username = $_SESSION['username'] ;
+    try {
+
+        $query = 'INSERT INTO "order"(username) VALUES (?) RETURNING order_id;';
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([$username]);
+        $orderId = $stmt->fetchColumn();
+        
+        $query = "SELECT product_id, quantity
+            FROM cart_item
+                    JOIN blocal.cart c ON cart_item.cart_id = c.cart_id
+            WHERE username = ?;";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([$username]);
+        $cartItems = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $query = "INSERT INTO order_item(product_id, order_id, quantity) VALUES (?,?,?);";
+        $stmt = $pdo->prepare($query);
+        
+        foreach ($cartItems as $cartItem) {
+            $stmt->execute([$cartItem['product_id'], $orderId,$cartItem['quantity']]);
+        };
+
+        $query = "DELETE FROM cart_item WHERE cart_id = ? ";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([$_SESSION['cart']]);
+
+        echo 'order placed sucesfully';
+
+        $pdo = null;
+        $stmt = null;
+        
+    } catch (\Throwable $th) {
+        die("Query failed: " . $e->getMessage());
+    }
+   
+} else {
+    header("Location: ../index.php");
+};
+
+           
