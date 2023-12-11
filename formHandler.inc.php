f16f6ba9026f284744b8bf702b324b200815fc63
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-11 16:34:18 +0100

    new product

--- formHandler.inc.php
+++ formHandler.inc.php
@@ -1,31 +1,47 @@
 <?php
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $productName = $_POST["productName"];
     $price = $_POST["price"];
     $stock = $_POST["stock"];
     $storeId = $_POST["storeId"];
+    $description = $_POST["description"];
+    $category = $_POST["categories"];
+    $file = $_FILES['image'];
+    $fileName = $_FILES['image']['name'];
+    $tmpLoc = $_FILES['image']['tmp_name'];
 
-    try {
-        require_once "dbh.inc.php";
 
-        $query = "INSERT INTO product(product_name, price, stock, store_id) VALUES (?,?,?,?);";
-        $stmt = $pdo->prepare($query);
+    if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
+        $targetDir = '../Images/'; // Specify the target directory
+        $targetForReading = 'Images/';
+        $targetFile = $targetDir . basename($fileName);
+        $targetFileDisplay = $targetForReading . basename($fileName);
 
-        $stmt->execute([$value1,$price,$stock,$store_id]);
-        echo "Record inserted successfully";
+        // Move the uploaded file to the target directory
+        if (move_uploaded_file($tmpLoc, $targetFile)) {
+            try {
+                require_once "dbh.inc.php";
 
-        $stmt->execute([$productName, $price, $stock, $store_id]);
+                $query = "INSERT INTO product(product_name, price, stock, store_id, description,category, image_path) VALUES (?,?,?,?,?,?,?);";
+                $stmt = $pdo->prepare($query);
 
-        $pdo = null;
-        $stmt = null;
+                $stmt->execute([$productName, $price, $stock, $storeId,$description,$category, $targetFileDisplay]);
 
-        header("Location: ../index.php");
-        die();
+                $pdo = null;
+                $stmt = null;
 
-    } catch (PDOException $e) {
-        die("Query failed" . $e->getMessage());
+                header("Location: ../index.php");
+                die();
+            } catch (PDOException $e) {
+                die("Query failed" . $e->getMessage());
+            }
+        } else {
+            echo 'Sorry, there was an error uploading your file.';
+        }
+    } else {
+        echo 'Error: ' . $_FILES['image']['error'];
     }
-}else{
+} else {
     header("Location: ../index.php");
 };
--- includes/formhandler.inc.php
+++ includes/formhandler.inc.php
@@ -1,30 +1,47 @@
 <?php
 
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    $productname = $_POST["productName"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $productName = $_POST["productName"];
     $price = $_POST["price"];
     $stock = $_POST["stock"];
     $storeId = $_POST["storeId"];
-    $imagePath =  $_POST["imagePath"];
+    $description = $_POST["description"];
+    $category = $_POST["categories"];
+    $file = $_FILES['image'];
+    $fileName = $_FILES['image']['name'];
+    $tmpLoc = $_FILES['image']['tmp_name'];
 
-    try {
-        require_once "dbh.inc.php";
 
-        $query = "INSERT INTO product (product_name, price, stock, store_id, image_path) VALUES (?, ?, ?, ?,?);";
+    if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
+        $targetDir = '../Images/'; // Specify the target directory
+        $targetForReading = 'Images/';
+        $targetFile = $targetDir . basename($fileName);
+        $targetFileDisplay = $targetForReading . basename($fileName);
 
-        $stmt = $pdo->prepare($query);
-        $stmt->execute([$productname, $price, $stock, $storeId, $imagePath]);
+        // Move the uploaded file to the target directory
+        if (move_uploaded_file($tmpLoc, $targetFile)) {
+            try {
+                require_once "dbh.inc.php";
 
-        $pdo = null;
-        $stmt = null;
+                $query = "INSERT INTO product(product_name, price, stock, store_id, description,category, image_path) VALUES (?,?,?,?,?,?,?);";
+                $stmt = $pdo->prepare($query);
 
-        header("Location: ../mainpagemanager.php");
+                $stmt->execute([$productName, $price, $stock, $storeId,$description,$category, $targetFileDisplay]);
 
-        die();
-    } catch (PDOException $e) {
-        die("Query failed: " . $e->getMessage());
+                $pdo = null;
+                $stmt = null;
+
+                header("Location: ../index.php");
+                die();
+            } catch (PDOException $e) {
+                die("Query failed" . $e->getMessage());
+            }
+        } else {
+            echo 'Sorry, there was an error uploading your file.';
+        }
+    } else {
+        echo 'Error: ' . $_FILES['image']['error'];
     }
-}
-else{
-    header("Location: ../mainpagemanager.php");
-}
+} else {
+    header("Location: ../index.php");
+};
