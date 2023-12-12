7e38351e8987751df07ea406599a4596bc9c124b
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-13 00:39:52 +0100

    nav

--- login.php
+++ login.php
@@ -10,9 +10,10 @@
 </head>
 <body style="margin: 0; background: #ffffff;">
 
-  <div class="container-fluid">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">
+  
+    <nav class="navbar navbar-custom navbar-expand-sm navbar-light fixed-top">
+    <div class="container-fluid">  
+    <a class="navbar-brand" href="#">
         <img src="Images/Blocal logo.png" width="30" height="30" alt="logo" class="img-fluid">
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
