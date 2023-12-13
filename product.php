bff901fb414bf440fd63afe2ccab9f32c0879457
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-13 12:34:31 +0100

    review

--- product.php
+++ product.php
@@ -137,12 +137,19 @@ $username = $_SESSION['username'];
 
         </div>
         <div class = "row">
+            
         <form action="" method="post">
+        <div class="form-group">
             <label for="review">Leave your review below:</label>
             <input id="review" type="text" name="review" placeholder="Review"><br>
+        </div>    
+        <div class="form-group">
             <label for="rate">Leave your rate below (1-5):</label>
             <input id="rate" type="text" name="rate" placeholder="Rate"><br>
+        </div>  
+        <div class="form-group">
             <input type="button" onclick="addReview();" name="add" value="Add review"  class='btn btn-primary '/>
+        </div>  
         </form>
         </div>
     </div>
