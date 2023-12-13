c287125d2303060406445d459f8eb0162353b411
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-13 18:16:41 +0100

    a

--- customerorders.php
+++ customerorders.php
@@ -145,7 +145,7 @@
         // Loop through each element with class "status"
         $(".status").each(function () {
             // Get the status value for each element
-            var status = $(this).data('customString');
+            var status = $(this).data('data-custom-string="value"');
             console.log("Status:", status);
 
             // Set the background color based on the status for each element
