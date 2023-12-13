38cf1e7131e70acbf6555d1b3fab7375383aa0e5
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-13 01:17:47 +0100

    a

--- editcustomerprofile.php
+++ editcustomerprofile.php
@@ -120,9 +120,28 @@
 
     ?>
     </div>
-        <button type="button" onclick="editProfile();" class="btn btn-primary" href = "customerprofile.php">Edit</button>
+        <button type="button" onclick="editProfile();" class="btn btn-primary" >Edit</button>
     </form>
 </div>
+
+<div class="modal fade" id="editSuccessModal" tabindex="-1" role="dialog" aria-labelledby="editSuccessModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editSuccessModalLabel">Changes Saved</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Your changes have been successfully saved.
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <script>
         function editProfile(){
             var username = $('input[name=username]').val();
