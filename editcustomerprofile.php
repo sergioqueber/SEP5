f32e3fa4535ed21d5bbc3e0e0f566a44ac122cad
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-13 01:26:03 +0100

    a

--- editcustomerprofile.php
+++ editcustomerprofile.php
@@ -129,7 +129,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="editSuccessModalLabel">Changes Saved</h5>
-                    <button type="button" class="close" data-dismiss="#editSuccessModal" aria-label="Close">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
