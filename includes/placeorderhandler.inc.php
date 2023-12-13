2ae5e7483118c1beb8ff1402d5f46cb2b1752c5c
marcintyminski <81477027+marcintyminski@users.noreply.github.com>
2023-12-13 16:47:57 +0100

    orders splitting

--- includes/placeorderhandler.inc.php
+++ includes/placeorderhandler.inc.php
@@ -4,38 +4,47 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_SESSION['username'] ;
     try {
         require_once "dbh.inc.php";
-        $query = 'INSERT INTO "order"(username, date_ordered, status) VALUES (?, CURRENT_DATE, ?) RETURNING order_id;';
-        $stmt = $pdo->prepare($query);
-        $stmt->execute([$username, 'Placed']);
-        $orderId = $stmt->fetchColumn();
-        
-        $query = "SELECT cart_item.product_id, quantity, price
-        FROM cart_item
-                 JOIN blocal.cart c ON cart_item.cart_id = c.cart_id
-                 JOIN blocal.product p ON cart_item.product_id = p.product_id
-        WHERE username = ?;";
+
+        $query = 'SELECT distinct store.store_id FROM store JOIN product p on store.store_id = p.store_id JOIN cart_item ci on p.product_id = ci.product_id JOIN cart c on c.cart_id = ci.cart_id WHERE username = ?;';
         $stmt = $pdo->prepare($query);
         $stmt->execute([$username]);
-        $cartItems = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-        $query = "INSERT INTO order_item(product_id, order_id, quantity) VALUES (?,?,?);";
-        $stmt = $pdo->prepare($query);
-        $totalprice = 0;
-        foreach ($cartItems as $cartItem) {
-            $stmt->execute([$cartItem['product_id'], $orderId,$cartItem['quantity']]);
-            $totalprice = $totalprice + ($cartItem['price']*$cartItem['quantity']);
-        };
+        foreach($results as $row){
+            $query = "SELECT cart_item.product_id, quantity, price
+            FROM cart_item
+                 JOIN blocal.cart c ON cart_item.cart_id = c.cart_id
+                 JOIN blocal.product p ON cart_item.product_id = p.product_id
+            WHERE username = ? AND store_id = ?;";
+            $stmt = $pdo->prepare($query);
+            $stmt->execute([$username, $row['store_id']]);
+            $cartItems = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+        
 
-        $query = 'UPDATE "order" SET total_price = ? WHERE order_id = ?;';
-        $stmt = $pdo->prepare($query);
-        $stmt->execute([$totalprice,$orderId ]);
+            $query = 'INSERT INTO "order"(username, date_ordered, status) VALUES (?, CURRENT_DATE, ?) RETURNING order_id;';
+            $stmt = $pdo->prepare($query);
+            $stmt->execute([$username, 'Placed']);
+            $orderId = $stmt->fetchColumn();
 
+            $query = "INSERT INTO order_item(product_id, order_id, quantity) VALUES (?,?,?);";
+            $stmt = $pdo->prepare($query);
+            $totalprice = 0;
+            foreach ($cartItems as $cartItem) {
+                $stmt->execute([$cartItem['product_id'], $orderId,$cartItem['quantity']]);
+                $totalprice = $totalprice + ($cartItem['price']*$cartItem['quantity']);
+            };
+
+            $query = 'UPDATE "order" SET total_price = ? WHERE order_id = ?;';
+            $stmt = $pdo->prepare($query);
+            $stmt->execute([$totalprice,$orderId ]);
+
+        
+        }
         $query = "DELETE FROM cart_item WHERE cart_id = ? ";
         $stmt = $pdo->prepare($query);
         $stmt->execute([$_SESSION['cart']]);
         header("Location: ../popup.php");
-
-    
 
         $pdo = null;
         $stmt = null;
@@ -47,3 +56,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 } else {
     header("Location: ../index.php");
 };
+
+           
