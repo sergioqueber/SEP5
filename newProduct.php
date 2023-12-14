651d42b30b42b8e6f493fa30c7541f7ba3e51cc1
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2023-12-14 19:37:34 +0100

    a

--- newProduct.php
+++ newProduct.php
@@ -85,8 +85,8 @@
                             <label for="category">Category</label>
                             <select class="form-select" id="category" name="category">
                                 <option value="Clothes" >Clothes</option>
-                                <option value="Shoes" >Clothes</option>
-                                <option value="Tech" >Clothes</option>
+                                <option value="Shoes" >Shoes</option>
+                                <option value="Tech" >Tech</option>
                                 <option value="" disabled selected>None</option>
                             </select>
                         </div>
