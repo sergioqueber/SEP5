7cef040cfa9bbc1ca24f43a5385fa9667452106c
sergioqueber <316341@via.dk>
2023-12-14 10:03:29 +0100

    Update addproducttocart.inc.php

--- addproducttocart.inc.php
+++ addproducttocart.inc.php
@@ -1,32 +1,32 @@
 <?php
 session_start();
-if(isset($_POST['quantity'])){
-$username = $_SESSION['username'];
-$productId = $_SESSION['product_id'];
-$quantityNew = $_POST['quantity'];
-try {
-            require_once "includes/dbh.inc.php";
-            $query1 = "SELECT quantity FROM cart_item WHERE cart_id = ? AND product_id = ?;";
-            $stmt1 = $pdo->prepare($query1);
-            $stmt1->execute([$_SESSION['cart'], $productId]);
-            $quantity = $stmt1->fetchColumn();
-            if($quantity === False || $quantity === NULL){
-                $query = "INSERT INTO cart_item(cart_id, product_id, quantity) VALUES (?,?,?);";
-                $stmt = $pdo->prepare($query);
-                $stmt->execute([$_SESSION['cart'], $productId, $quantityNew]);
-            }else{
-                $query = "UPDATE cart_item SET quantity = ? WHERE cart_id = ? AND product_id = ?;";
-                $stmt = $pdo->prepare($query);
-                $stmt->execute([$quantityNew + $quantity, $_SESSION['cart'], $productId]);
-            }
-            
-            
-
-            $pdo = null;
-            $stmt = null;
-        } catch (PDOException $e) {
-            die("Query failed: " . $e->getMessage());
+if (isset($_POST['quantity'])) {
+    $username = $_SESSION['username'];
+    $productId = $_SESSION['product_id'];
+    $quantityNew = $_POST['quantity'];
+    try {
+        require_once "includes/dbh.inc.php";
+        $query1 = "SELECT quantity FROM cart_item WHERE cart_id = ? AND product_id = ?;";
+        $stmt1 = $pdo->prepare($query1);
+        $stmt1->execute([$_SESSION['cart'], $productId]);
+        $quantity = $stmt1->fetchColumn();
+        if ($quantity === False || $quantity === NULL) {
+            $query = "INSERT INTO cart_item(cart_id, product_id, quantity) VALUES (?,?,?);";
+            $stmt = $pdo->prepare($query);
+            $stmt->execute([$_SESSION['cart'], $productId, $quantityNew]);
+        } else {
+            $query = "UPDATE cart_item SET quantity = ? WHERE cart_id = ? AND product_id = ?;";
+            $stmt = $pdo->prepare($query);
+            $stmt->execute([$quantityNew + $quantity, $_SESSION['cart'], $productId]);
         }
-    }else{
-        header("Location: ../index.php");
-    };
+
+
+
+        $pdo = null;
+        $stmt = null;
+    } catch (PDOException $e) {
+        die("Query failed: " . $e->getMessage());
+    }
+} else {
+    header("Location: ../index.php");
+};
